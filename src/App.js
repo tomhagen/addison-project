067ce038d91b7cb0df5bf844550cda6319de0852
tomhagen <tom@mrs.com.vn>
2019-06-25 13:48:24 +0700

    Setup full Begin: Antd SCSS Resetcss wowAnimation fontGoogle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment} from 'react';
+import './App.scss';
+import 'antd/dist/antd.css';
+import {Button} from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Button type="primary" >Primary Button</Button>
+      <h1>Welcome to xediek</h1>
+    </Fragment>
   );
 }
 
